55916cda54d69d2bbe2a39c373da7f5879837018
dex <121840104+dex4tw@users.noreply.github.com>
2025-10-31 20:32:24 -0400

    yap stfo anticheat blocker

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/36a0ea7f8fec629eee9d01d38c3a621303cf3bbf/Brew%2520Reach.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/cba9a6f44b7b7df67fc72575b2af21dd55c1952d/Brew%2520Reach.lua"))()
