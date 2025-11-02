6bdf0d2e1e7000bf4005fafb40580f67296d5011
dex <121840104+dex4tw@users.noreply.github.com>
2025-11-01 21:38:51 -0400

    stfo:abtb bypass

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/8964f6316669828d5215603cc927bffa53268f87/Brew%2520Reach.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/c91fc0f81aa349141829ce478d6c60ceeeaf3f32/Brew%2520Reach.lua"))()
