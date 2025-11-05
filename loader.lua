2e676eb2bcfb3c2faf1e382a7a14456ecf318606
dex <121840104+dex4tw@users.noreply.github.com>
2025-11-04 20:02:52 -0500

    bleh

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/c91fc0f81aa349141829ce478d6c60ceeeaf3f32/Brew%2520Reach.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/d9c46a19b9cfd8315ebcc1ca2d7dc97645fb9b3b/Brew%2520Reach.lua"))()
