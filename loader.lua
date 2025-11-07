70356637fd3908b5dc181610fff8ff4ba8375965
dex <121840104+dex4tw@users.noreply.github.com>
2025-11-07 16:30:03 -0500

    freeze fix

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/d24dda195ffe1df0a1cbe80789cce1f6efff8ea6/Brew%2520Reach.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/982baa05e9625715c22b88eedb8172c98103dcc3/Brew%2520Reach.lua"))()
