207f643a0d7ec24a73e38f6cc44ef9d693029987
dex <121840104+dex4tw@users.noreply.github.com>
2025-11-16 18:10:03 -0500

    disconnect fix

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/c2af614a7a8a42f29867b2ce35674098b7ad5177/Brew%2520Reach.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/dex4tw/6642fdf5d0199104e308c5f392557f75/raw/aecf58bbb73c5ff7d4c2b4e0c12302ed02005eb8/Brew%2520Reach.lua"))()
